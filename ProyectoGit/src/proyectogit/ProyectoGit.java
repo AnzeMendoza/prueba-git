8feb63560f5e1eee7617532e673895f4d40cba70
Sucho <anze.mendoza@gmail.com>
2021-09-29 20:03:16 -0300

    update nueva

--- ProyectoGit/src/proyectogit/ProyectoGit.java
+++ ProyectoGit/src/proyectogit/ProyectoGit.java
@@ -6,6 +6,7 @@ public class ProyectoGit {
     public static void main(String[] args) {
         // Hola! Andrea Reyna.
         System.out.println("Hola").
+        System.out.println("Hola desde rama 2").
     }
     
 }
